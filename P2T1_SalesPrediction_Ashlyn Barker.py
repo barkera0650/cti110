20533a1ecf1b6f6205cb4fc921c53706108508c6
barkera0650 <55783685+barkera0650@users.noreply.github.com>
2019-09-25 04:27:22 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_Ashlyn Barker.py
@@ -0,0 +1,10 @@
+#Get the projected total sales over time.
+# 9/25/2019
+# CTI-110 P2T1 - Sales Prediction
+# Ashlyn Barker
+total_sales=float(input('Enter the projected sales:'))
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit,',.2f'))
---
+++ P2_T1_SalesPrediction_Ashlyn Barker.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Ed Barker/Documents/P2_SalesPrediction_Ashlyn Barker.py =
+Enter the projected sales:1000
+The profit is $ 230.0
+>>> 
+= RESTART: C:/Users/Ed Barker/Documents/P2_SalesPrediction_Ashlyn Barker.py =
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+= RESTART: C:/Users/Ed Barker/Documents/P2_SalesPrediction_Ashlyn Barker.py =
+Enter the projected sales:10000
+The profit is $ 2,300.00
+>>> 
